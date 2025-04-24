d5e7f8ff1be352788578568df7689a3590420194
migangver <33725012+migangver@users.noreply.github.com>
2025-04-23 22:35:09 -0400

    Update triaje_bot.py

--- triaje_bot.py
+++ triaje_bot.py
@@ -94,4 +94,6 @@ def whatsapp_bot():
     return respuesta
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
